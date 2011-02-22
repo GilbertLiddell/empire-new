6514b01851e503aa43973b72fc18c466c9c75f91
unknown <gliddell@.totalrepair.co.uk>
2011-02-22 15:36:32 +0000

    fixed songs not showing

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,7 +1,7 @@
 class MusicController < ApplicationController
 
   def index
-    @song_headers = Song.all(:order => "sortorder", :group => "decade")
+    @song_headers = Song.all(:order => "id", :group => "decade")
     @songs = Song.all(:order => "sortorder")
   end
 end
